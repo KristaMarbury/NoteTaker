021f33e2459fc8511c782b62f60e9cb3a0570dbb
Krista Marbury <laetaiustus@gmail.com>
2021-06-10 12:43:11 -0500

    fiddling with the apiroutes file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,12 +6,21 @@ const store = require("../db/store");
 //call class store in here 
 
 // GET "/api/notes" responds with all notes from the database
-router.get()
+router.get("/api/notes", function (req, res) {
+  res.send("notes");
+});
+
 // POST "/api/notes" posts the note to the database
-router.post()
+router.post('/api/notes', function (req, res) {
+    res.send('Your note is safe with me!')
+});
 // DELETE "/api/notes" deletes the note with an id equal to req.params.id
-router.delete()
+router.delete('/api/notes', function (req, res) {
+    res.send('And like that it is gone!')
+});
 
 
 //stays at the bottom
 module.exports = router;
+
+//use uuid! helps with automation
