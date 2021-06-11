e68a1aade3fc165d8c4e8c41a401ca1bac71c24c
Krista Marbury <laetaiustus@gmail.com>
2021-06-11 14:45:49 -0500

    slimmed down the apiroutes and htmlroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,37 +1,25 @@
 const router = require("express").Router();
-const store = require("../db/store");
-
-//use router var (router.get, post, delete) 
+const store = require("../db/store.js");
+const notesData = require("../routes/htmlRoutes");
 
 //call class store in here 
 module.exports = (app) => {
- 
   app.get("/notes", (req, res) => res.json(notesData));
 
-  // app.get("/api/waitlist", (req, res) => res.json(waitListData));
+  // GET "/api/notes" responds with all notes from the database
+  router.get("/api/notes", function (req, res) {
+    res.send("notes");
+  });
 
-  app.post("/api/clear", (req, res) => {
-    // Empty out the arrays of data
-    tableData.length = 0;
-    waitListData.length = 0;
-
-    res.json({ ok: true });
+  // POST "/api/notes" posts the note to the database
+  router.post('/api/notes', function (req, res) {
+      res.send('Your note is safe with me!')
+  });
+  // DELETE "/api/notes" deletes the note with an id equal to req.params.id
+  router.delete('/api/notes', function (req, res) {
+      res.send('And like that it is gone!')
   });
 };
-
-// GET "/api/notes" responds with all notes from the database
-// router.get("/api/notes", function (req, res) {
-//   res.send("notes");
-// });
-
-// // POST "/api/notes" posts the note to the database
-// router.post('/api/notes', function (req, res) {
-//     res.send('Your note is safe with me!')
-// });
-// // DELETE "/api/notes" deletes the note with an id equal to req.params.id
-// router.delete('/api/notes', function (req, res) {
-//     res.send('And like that it is gone!')
-// });
 
 
 //stays at the bottom
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,42 +4,15 @@ const router = require("express").Router();
 //html routes are only going to be router.gets
 
 //get for the index.html 
-router.get()
+ router.get("/", (req, res) => {
+   res.sendFile(path.join(__dirname, "../public/index.html"));
+ });
 
-//get for the note.html
-router.get()
-
+ //get for the note.html
+ router.get("/notes", (req, res) => {
+   res.sendFile(path.join(__dirname, "../public/notes.html"));
+ });
 
 //stays at the bottom
 module.exports = router;
 
-//maybe will need this 
-// Routes
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "index.html"));
-// });
-
-// app.get("/notes", (req, res) => {
-//   res.sendFile(path.join(__dirname, "notes.html"));
-// });
-
-// //get our entire database
-// app.get("/api/notes", (req, res) => {
-//   return res.json(note);
-// });
-
-// //gets a specific character from the database
-// app.get("/api/notes/:thatone", (req, res) => {
-//   const chosen = req.params.note;
-//   const chosenNote =
-//     note.find((thatone) => thatone.routeName == chosen) || false;
-//   return res.json(chosenNote);
-// });
-
-// //create a new character
-// app.post("/api/newnote", (req, res) => {
-//   const newThought = req.body;
-//   newThought.routeName = newThought.name.replace(/\s+/g, "").toLowerCase();
-//   note.push(newThought);
-//   res.json(newThought);
-// });
